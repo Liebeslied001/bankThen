7d1407e9d76e955efb83be2d6f67d052f97d2ae0
joseph <josepharaujo123@gmail.com>
2023-01-10 15:25:46 -0500

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
+import { CreateNewCategoryComponent } from './components/create-new-category/create-new-category.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionsComponent
+    TransactionsComponent,
+    CreateNewCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Transaction } from 'src/app/models/transaction.model';
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent {
-
+  @Input() transaction: Transaction = {
+    id: 0,
+    icon: "",
+    category: "",
+    amount: 0
+    }
 }
