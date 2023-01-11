3e0cc2ba8b26cec8328f05d80d7074ec1d3ddcb0
AntonyJr00 <antonyrodl20@gmail.com>
2023-01-11 08:56:22 -0500

    alguito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { CreateNewCategoryComponent } from './components/create-new-category/create-new-category.component';
 import { FormsModule } from '@angular/forms';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionsComponent,
-    CreateNewCategoryComponent
+    CreateNewCategoryComponent,
+    SideBarComponent,
+    ButtonComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/create-new-category/create-new-category.component.ts
+++ src/app/components/create-new-category/create-new-category.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-new-category',
   templateUrl: './create-new-category.component.html',
-  styleUrls: ['./create-new-category.component.css']
+  styleUrls: ['./create-new-category.component.css'],
 })
 export class CreateNewCategoryComponent {
-
+  color: string = '#0CB7D5';
+  money: number = 0;
+  textSalario: string = '';
 }
